48b5bd90811f94edbe48b738c370f890fd609b0e
Allen Huffman <huffmancharles14@gmail.com>
2023-09-28 12:34:27 -0500

    add function to play a round of rock, paper, scissors. Add function to play 5 rounds of rock, paper, scissors.

--- index.js
+++ index.js
@@ -1,12 +1,56 @@
-console.log("Hello World");
-
 const choiceArr = ["rock", "paper", "scissors"];
 
 const getComputerChoice = () => {
   const choice = choiceArr[Math.floor(Math.random() * 3)];
   return choice;
 };
-console.log(getComputerChoice());
+
+const playRound = (playerSelection, computerSelection) => {
+  /* we want the logic of this function to lowercase the playerSelection input
+  so if the player inputs ROCK rock will be returned.
+  we also want to create a conditional for the case of player chooses rock
+  and computer chooses paper to return you lose paper beats rock.
+  */
+
+  playerSelection = prompt(
+    "Lets play a game of Rock, Paper, Scissors"
+  ).toLowerCase();
+
+  computerSelection = getComputerChoice();
+
+  console.log("computer selected:", computerSelection);
+  console.log("player selected:", playerSelection);
+
+  const winMessage = `You Win! ${computerSelection} beats ${playerSelection}`;
+  const loseMessage = `You Lose! ${computerSelection} loses to ${playerSelection}`;
+  const tieMessage = `Tie! you both choose ${computerSelection}`;
+
+  if (computerSelection === playerSelection) {
+    return tieMessage;
+  } else if (computerSelection === "rock" && playerSelection === "paper") {
+    return winMessage;
+  } else if (computerSelection === "rock" && playerSelection === "scissors") {
+    return loseMessage;
+  } else if (computerSelection === "paper" && playerSelection === "rock") {
+    return loseMessage;
+  } else if (computerSelection === "paper" && playerSelection === "scissors") {
+    return winMessage;
+  } else if (computerSelection === "scissors" && playerSelection === "rock") {
+    return winMessage;
+  } else if (computerSelection === "scissors" && playerSelection === "paper") {
+    return loseMessage;
+  }
+};
+
+const game = () => {
+  playRound();
+  playRound();
+  playRound();
+  playRound();
+  playRound();
+};
+
+console.log(playRound());
 /*
 We are going to build a rock paper scissors application that is
 played in the console.
